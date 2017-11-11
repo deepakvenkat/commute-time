d05981c0407cacf864b99677dc055adbd4b38db4
Deepak Venkatachalam <dvenkatachalam@shutterstock.com>
2017-11-11 18:38:58 -0500

    Add a train count component.

---
+++ src/TrainCountComponent.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import {FormControl, ControlLabel, InputGroup, Glyphicon} from 'react-bootstrap';
+
+import './TrainCountComponent.css';
+
+class TrainCountComponent extends Component {
+  static propTypes = {
+    trainCountChangeHandler: PropTypes.func,
+    trainCount: PropTypes.number,
+  };
+
+  static defaultPropTypes = {
+    trainCountChangeHandler: (() => {}),
+    trainCount: 0,
+  };
+
+  render() {
+    const {
+      trainCountChangeHandler,
+      trainCount,
+    } = this.props;
+
+    return(
+      <div className="train-count-wrapper">
+      <ControlLabel> Number of Trains</ControlLabel>
+        <InputGroup
+          className="train-count-group"
+        >
+          <InputGroup.Addon
+            onClick={() => trainCountChangeHandler(false)}
+          >
+            <Glyphicon glyph='minus-sign'/>
+          </InputGroup.Addon>
+          <FormControl
+            className="train-count-input"
+            type="text"
+            value={trainCount}
+            readOnly
+          />
+          <InputGroup.Addon
+            onClick = {() => trainCountChangeHandler(true)}
+          >
+            <Glyphicon glyph='plus-sign'/>
+          </InputGroup.Addon>
+        </InputGroup>
+      </div>
+    );
+  }
+}
+
+export default TrainCountComponent;
